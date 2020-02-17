3fc5a7a7c823044743754e97c6ba23ddbea709ee
JulieSweany <31965277+JulieSweany@users.noreply.github.com>
2020-02-16 20:43:01 -0500

    Uploading files for exercise05-1

---
+++ ch05_ex1/product_catalog/categories_nav.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<nav>
+<ul>
+    <!-- display links for all categories -->
+    <?php foreach($categories as $category) : ?>
+    <li>
+        <a href="?category_id=<?php echo $category['categoryID']; ?>">
+        <?php echo $category['categoryName']; ?>
+        </a>
+    </li>
+    <?php endforeach; ?>
+</ul>
+</nav>
+
+
+
